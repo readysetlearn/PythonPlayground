60dc3ae95cd092bba027f06029f5d20ee8a8fdf8
Josh <mister_josh@ymail.com>
2017-04-14 02:05:39 -0700

    Refactored code to be more "pythonic"

--- CountVowels.py
+++ CountVowels.py
@@ -1,11 +1,19 @@
-# Counts vowels in user input
+# Counts vowels in user supplied sentence
+def do_count(usr_str):
+    """Returns number of vowels"""
+    include_y = input('Is \'y\' considered a vowel? (y/n)')
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    if include_y == 'y' or include_y == 'yes':
+        vowels.append('y')
+    count = 0
+    for v in vowels:
+        count += usr_str.count(v)
+    return count
 
-sentence = input('Give me a sentence: ')
-include_y = input('Is \'y\' considered a vowel? (y/n)')
-vowels = ['a', 'e', 'i', 'o', 'u']
-if include_y == 'y' or include_y == 'yes':
-    vowels.append('y')
-count = 0;
-for v in vowels:
-    count += sentence.count(v)
-print('There are %d vowelss.' % count)
+if __name__ == "__main__":
+    sentence = input('Give me a sentence: ')
+    counted = do_count(sentence)
+    print('There are {} vowels in "{}".'.format(counted, sentence))
+
+
+
