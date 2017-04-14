5a93889a95a9a157261c1bcddc57efa8bdd61cef
Josh <mister_josh@ymail.com>
2017-04-14 02:58:11 -0700

    Program repeats until exit is entered.

--- CountVowels.py
+++ CountVowels.py
@@ -1,4 +1,18 @@
-# Counts vowels in user supplied sentence
+# Counts vowels in user supplied sentences
+
+def main():
+    total_sentences = total_vowels = 0;
+    while True:
+        sentence = input('Give me a sentence or type exit to exit: ')
+        if sentence == 'exit':
+            break
+        counted = do_count(sentence)
+        print('There are {} vowels in "{}".'.format(counted, sentence))
+        total_sentences += 1
+        total_vowels += counted
+    print('Total number of sentences processed: {}. Total vowels: {}.'.format(total_sentences, total_vowels))
+
+
 def do_count(usr_str):
     """Returns number of vowels"""
     include_y = input('Is \'y\' considered a vowel? (y/n)')
@@ -10,10 +24,6 @@ def do_count(usr_str):
         count += usr_str.count(v)
     return count
 
+
 if __name__ == "__main__":
-    sentence = input('Give me a sentence: ')
-    counted = do_count(sentence)
-    print('There are {} vowels in "{}".'.format(counted, sentence))
-
-
-
+    main()
